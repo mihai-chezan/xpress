de7d73e3e5689b0d8b52b6467ac4d54d28934cb5
partacc <cpartac@gmail.com>
2013-10-24 21:15:17 +0300

    added controller for path "graphs/tags"

--- src/main/java/xpress/XpressService.java
+++ src/main/java/xpress/XpressService.java
@@ -14,21 +14,22 @@ import com.yammer.dropwizard.config.Environment;
  */
 public class XpressService extends Service<XpressConfiguration> {
 
-	public static void main(String[] args) throws Exception {
-		new XpressService().run(args);
-	}
+    public static void main(String[] args) throws Exception {
+        new XpressService().run(args);
+    }
 
-	@Override
-	public void initialize(Bootstrap<XpressConfiguration> bootstrap) {
-		bootstrap.setName("Xpress");
-		bootstrap.addBundle(new AssetsBundle("/assets/", "/"));
-	}
+    @Override
+    public void initialize(Bootstrap<XpressConfiguration> bootstrap) {
+        bootstrap.setName("Xpress");
+        bootstrap.addBundle(new AssetsBundle("/assets/", "/"));
+    }
 
-	@Override
-	public void run(XpressConfiguration configuration, Environment environment) {
-		environment.addResource(new TagCloudResource());
-		environment.addResource(new VoteResource());
-		environment.addResource(new MoodGraphResource());
-	}
+    @Override
+    public void run(XpressConfiguration configuration, Environment environment) {
+        environment.addResource(new TagCloudResource());
+        environment.addResource(new VoteResource());
+        environment.addResource(new MoodGraphResource());
+        environment.addResource(new GraphsTagsResource());
+    }
 
 }
