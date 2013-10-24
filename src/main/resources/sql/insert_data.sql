2cfae5b3c4cf45b6a548e7d1c972863b8f102534
sechelc <cristian.sechel@yahoo.ro>
2013-10-25 02:30:45 +0300

    created scrpits for populating the db

---
+++ src/main/resources/sql/insert_data.sql
@@ -0,0 +1,8 @@
+INSERT INTO VoteEntity(TAG, MOOD, TIME) values ('ala','HAPPY',10);
+INSERT INTO VoteEntity(TAG, MOOD, TIME) values ('alabala','HAPPY',10);
+INSERT INTO VoteEntity(TAG, MOOD, TIME) values ('portocala','HAPPY',11111);
+INSERT INTO VoteEntity(TAG, MOOD, TIME) values ('lolaie','HAPPY',2020);
+
+INSERT INTO VoteEntity(TAG, MOOD, TIME) values ('trist','UNHAPPY',3030);
+INSERT INTO VoteEntity(TAG, MOOD, TIME) values ('atatdetrist','UNHAPPY',30303);
+INSERT INTO VoteEntity(TAG, MOOD, TIME) values ('siMaiTrist','UNHAPPY',303030303);
