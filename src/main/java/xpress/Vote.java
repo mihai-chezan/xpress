f05dc285b560910d1d53343b6f79b67288da95a3
Mihai Chezan <mihai.chezan@softgress.com>
2013-10-24 17:32:00 +0300

    fix Vote

--- src/main/java/xpress/Vote.java
+++ src/main/java/xpress/Vote.java
@@ -3,15 +3,20 @@
  */
 package xpress;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+
 /**
  * @author mcq
  *
  */
 public class Vote {
+	@JsonProperty
 	private final Mood mood;
+	@JsonProperty
 	private final String tag;
 	
-	public Vote(Mood mood, String tag) {
+	public Vote(@JsonProperty("mood") Mood mood, @JsonProperty("tag") String tag) {
 		super();
 		this.mood = mood;
 		this.tag = tag;
--- src/main/java/xpress/VoteResponse.java
+++ src/main/java/xpress/VoteResponse.java
@@ -3,6 +3,7 @@
  */
 package xpress;
 
+
 /**
  * @author mcq
  *
