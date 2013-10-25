907c58fb14057436e01b67ee3d2f9f464d7b78f5
sechelc <cristian.sechel@yahoo.ro>
2013-10-25 16:01:44 +0300

    added a multiple pie chart

---
+++ src/main/resources/assets/multiplePieChart.js
@@ -0,0 +1,59 @@
+$(function () {
+
+    var lalala =  function(){
+        $.getJSON("http://localhost:9090/service/graphs/tags/similar/"+"fedex"+"/LAST_MONTH", function(data){
+            for (var i = 0; i < data.length; i++) {
+                chartOptions.series = data[i].series;
+                var e = '#'+'container' + i;
+                $(e).highcharts(chartOptions);
+            }
+        })
+    }
+
+    var chartOptions = {
+        chart: {
+            plotBackgroundColor: null,
+            plotBorderWidth: null,
+            plotShadow: false
+        },
+        title: {
+            text: 'Browser market shares at a specific website, 2010'
+        },
+        tooltip: {
+            pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+        },
+        plotOptions: {
+            pie: {
+                allowPointSelect: true,
+                cursor: 'pointer',
+                dataLabels: {
+                    enabled: true,
+                    color: '#000000',
+                    connectorColor: '#000000',
+                    format: '<b>{point.name}</b>: {point.percentage:.1f} %'
+                }
+            }
+        },
+        series: [{
+            type: 'pie',
+            name: 'Browser share',
+            data: [
+                ['Firefox',   45.0],
+                ['IE',       26.8],
+                {
+                    name: 'Chrome',
+                    y: 12.8,
+                    sliced: true,
+                    selected: true
+                },
+                ['Safari',    8.5],
+                ['Opera',     6.2],
+                ['Others',   0.7]
+            ]
+        }]
+    }
+    lalala();
+
+});
+
+
