9e5a33f518bb2fde06273122c2e699c101eab72c
sechelc <cristian.sechel@yahoo.ro>
2013-10-25 11:53:06 +0300

    added data in the db for the last week

--- src/main/resources/sql/insert_data.sql
+++ src/main/resources/sql/insert_data.sql
@@ -1,8 +1,93 @@
-INSERT INTO VoteEntity(TAG, MOOD, TIME) values ('ala','HAPPY',10);
-INSERT INTO VoteEntity(TAG, MOOD, TIME) values ('alabala','HAPPY',10);
-INSERT INTO VoteEntity(TAG, MOOD, TIME) values ('portocala','HAPPY',11111);
-INSERT INTO VoteEntity(TAG, MOOD, TIME) values ('lolaie','HAPPY',2020);
+INSERT INTO VoteEntity(TAG, MOOD, TIME) values ('won fedex','HAPPY',1382602794185); /* azi*/
+INSERT INTO VoteEntity(TAG, MOOD, TIME) values ('won fedex','HAPPY',1382602794184); /* azi*/
+INSERT INTO VoteEntity(TAG, MOOD, TIME) values ('won fedex','HAPPY',1382602794183); /* azi*/
+INSERT INTO VoteEntity(TAG, MOOD, TIME) values ('won fedex','HAPPY',1382602794182); /* azi*/
+INSERT INTO VoteEntity(TAG, MOOD, TIME) values ('won fedex','HAPPY',1382602794481); /* azi*/
+INSERT INTO VoteEntity(TAG, MOOD, TIME) values ('won fedex','HAPPY',1382602794141); /* azi*/
+INSERT INTO VoteEntity(TAG, MOOD, TIME) values ('bonus','HAPPY',1382602794151); /* azi*/
+INSERT INTO VoteEntity(TAG, MOOD, TIME) values ('bouns','HAPPY',1382602794161); /* azi*/
+INSERT INTO VoteEntity(TAG, MOOD, TIME) values ('bonus','HAPPY',1382602794171); /* azi*/
+INSERT INTO VoteEntity(TAG, MOOD, TIME) values ('bonus','HAPPY',1382602794171); /* azi*/
+INSERT INTO VoteEntity(TAG, MOOD, TIME) values ('sarmale','HAPPY',1382602794171); /* azi*/
+INSERT INTO VoteEntity(TAG, MOOD, TIME) values ('sarmale','HAPPY',1382602794171); /* azi*/
+INSERT INTO VoteEntity(TAG, MOOD, TIME) values ('sarmale','HAPPY',1382602794171); /* azi*/
+INSERT INTO VoteEntity(TAG, MOOD, TIME) values ('lost at foosball','UNHAPPY',1382602794171); /* azi*/
+INSERT INTO VoteEntity(TAG, MOOD, TIME) values ('lost at foosball','UNHAPPY',1382602794171); /* azi*/
+INSERT INTO VoteEntity(TAG, MOOD, TIME) values ('lost at foosball','UNHAPPY',1382602794171); /* azi*/
+INSERT INTO VoteEntity(TAG, MOOD, TIME) values ('lost at foosball','UNHAPPY',1382602794171); /* azi*/
 
-INSERT INTO VoteEntity(TAG, MOOD, TIME) values ('trist','UNHAPPY',3030);
-INSERT INTO VoteEntity(TAG, MOOD, TIME) values ('atatdetrist','UNHAPPY',30303);
-INSERT INTO VoteEntity(TAG, MOOD, TIME) values ('siMaiTrist','UNHAPPY',303030303);
+
+INSERT INTO VoteEntity(TAG, MOOD, TIME) values ('won at foosball','HAPPY',1382516398186); /*ieri*/
+INSERT INTO VoteEntity(TAG, MOOD, TIME) values ('won at foosball','HAPPY',1382516394186); /*ieri*/
+INSERT INTO VoteEntity(TAG, MOOD, TIME) values ('won at foosball','HAPPY',1382516397186); /*ieri*/
+INSERT INTO VoteEntity(TAG, MOOD, TIME) values ('fedex','HAPPY',1382516396186); /*ieri*/
+INSERT INTO VoteEntity(TAG, MOOD, TIME) values ('fedex','HAPPY',1382516396186); /*ieri*/
+INSERT INTO VoteEntity(TAG, MOOD, TIME) values ('fedex','HAPPY',1382516396186); /*ieri*/
+INSERT INTO VoteEntity(TAG, MOOD, TIME) values ('fedex','HAPPY',1382516396186); /*ieri*/
+INSERT INTO VoteEntity(TAG, MOOD, TIME) values ('fedex','HAPPY',1382516396186); /*ieri*/
+INSERT INTO VoteEntity(TAG, MOOD, TIME) values ('fedex','HAPPY',1382516396186); /*ieri*/
+INSERT INTO VoteEntity(TAG, MOOD, TIME) values ('fedex','HAPPY',1382516396186); /*ieri*/
+INSERT INTO VoteEntity(TAG, MOOD, TIME) values ('fedex','HAPPY',1382516396186); /*ieri*/
+INSERT INTO VoteEntity(TAG, MOOD, TIME) values ('rosia montana','UNHAPPY',1382516374186); /*ieri*/
+INSERT INTO VoteEntity(TAG, MOOD, TIME) values ('rosia montana','UNHAPPY',1382516384186); /*ieri*/
+INSERT INTO VoteEntity(TAG, MOOD, TIME) values ('rosia montana','UNHAPPY',1382516384186); /*ieri*/
+INSERT INTO VoteEntity(TAG, MOOD, TIME) values ('rosia montana','UNHAPPY',1382516384186); /*ieri*/
+INSERT INTO VoteEntity(TAG, MOOD, TIME) values ('rosia montana','UNHAPPY',1382516384186); /*ieri*/
+
+
+INSERT INTO VoteEntity(TAG, MOOD, TIME) values ('got promoted','HAPPY',1382429994186); /*acum 2 zile*/
+INSERT INTO VoteEntity(TAG, MOOD, TIME) values ('got promoted','HAPPY',1382429994186); /*acum 2 zile*/
+INSERT INTO VoteEntity(TAG, MOOD, TIME) values ('got promoted','HAPPY',1382429994186); /*acum 2 zile*/
+INSERT INTO VoteEntity(TAG, MOOD, TIME) values ('got promoted','HAPPY',1382429994186); /*acum 2 zile*/
+INSERT INTO VoteEntity(TAG, MOOD, TIME) values ('won at foosball','HAPPY',1382429994186); /*acum 2 zile*/
+INSERT INTO VoteEntity(TAG, MOOD, TIME) values ('won at foosball','HAPPY',1382429994186); /*acum 2 zile*/
+INSERT INTO VoteEntity(TAG, MOOD, TIME) values ('won at foosball','HAPPY',1382429994186); /*acum 2 zile*/
+INSERT INTO VoteEntity(TAG, MOOD, TIME) values ('won at foosball','HAPPY',1382429994186); /*acum 2 zile*/
+INSERT INTO VoteEntity(TAG, MOOD, TIME) values ('lost at foosball','UNHAPPY',1382429994186); /*acum 2 zile*/
+INSERT INTO VoteEntity(TAG, MOOD, TIME) values ('lost at foosball','UNHAPPY',1382429994186); /*acum 2 zile*/
+INSERT INTO VoteEntity(TAG, MOOD, TIME) values ('lost at foosball','UNHAPPY',1382429994186); /*acum 2 zile*/
+INSERT INTO VoteEntity(TAG, MOOD, TIME) values ('lost at foosball','UNHAPPY',1382429994186); /*acum 2 zile*/
+INSERT INTO VoteEntity(TAG, MOOD, TIME) values ('rosia montana','UNHAPPY',1382429994186); /*acum 2 zile*/
+INSERT INTO VoteEntity(TAG, MOOD, TIME) values ('rosia montana','UNHAPPY',1382429994186); /*acum 2 zile*/
+INSERT INTO VoteEntity(TAG, MOOD, TIME) values ('rosia montana','UNHAPPY',1382429994186); /*acum 2 zile*/
+INSERT INTO VoteEntity(TAG, MOOD, TIME) values ('rosia montana','UNHAPPY',1382429994186); /*acum 2 zile*/
+
+INSERT INTO VoteEntity(TAG, MOOD, TIME) values ('sarmale','HAPPY',1382343594186);  /*acum 3 zile*/
+INSERT INTO VoteEntity(TAG, MOOD, TIME) values ('sarmale','HAPPY',1382343594186);  /*acum 3 zile*/
+INSERT INTO VoteEntity(TAG, MOOD, TIME) values ('sarmale','HAPPY',1382343594186);  /*acum 3 zile*/
+INSERT INTO VoteEntity(TAG, MOOD, TIME) values ('sarmale','HAPPY',1382343594186);  /*acum 3 zile*/
+INSERT INTO VoteEntity(TAG, MOOD, TIME) values ('sarmale','HAPPY',1382343594186);  /*acum 3 zile*/
+INSERT INTO VoteEntity(TAG, MOOD, TIME) values ('sarmale','HAPPY',1382343594186);  /*acum 3 zile*/
+INSERT INTO VoteEntity(TAG, MOOD, TIME) values ('sarmale','HAPPY',1382343594186);  /*acum 3 zile*/
+INSERT INTO VoteEntity(TAG, MOOD, TIME) values ('sarmale','HAPPY',1382343594186);  /*acum 3 zile*/
+INSERT INTO VoteEntity(TAG, MOOD, TIME) values ('sarmale','UNHAPPY',1382343594186);  /*acum 3 zile*/
+INSERT INTO VoteEntity(TAG, MOOD, TIME) values ('rosia montana','UNHAPPY',1382343594186);  /*acum 3 zile*/
+INSERT INTO VoteEntity(TAG, MOOD, TIME) values ('rosia montana','UNHAPPY',1382343594186);  /*acum 3 zile*/
+INSERT INTO VoteEntity(TAG, MOOD, TIME) values ('rosia montana','UNHAPPY',1382343594186);  /*acum 3 zile*/
+
+INSERT INTO VoteEntity(TAG, MOOD, TIME) values ('perforce was down','UNHAPPY',1382257194186);  /*acum 4 zile*/
+INSERT INTO VoteEntity(TAG, MOOD, TIME) values ('perforce was down','UNHAPPY',1382257194186);  /*acum 4 zile*/
+INSERT INTO VoteEntity(TAG, MOOD, TIME) values ('perforce was down','UNHAPPY',1382257194186);  /*acum 4 zile*/
+INSERT INTO VoteEntity(TAG, MOOD, TIME) values ('perforce was down','UNHAPPY',1382257194186);  /*acum 4 zile*/
+INSERT INTO VoteEntity(TAG, MOOD, TIME) values ('perforce was down','UNHAPPY',1382257194186);  /*acum 4 zile*/
+INSERT INTO VoteEntity(TAG, MOOD, TIME) values ('perforce was down','UNHAPPY',1382257194186);  /*acum 4 zile*/
+INSERT INTO VoteEntity(TAG, MOOD, TIME) values ('perforce was down','UNHAPPY',1382257194186);  /*acum 4 zile*/
+INSERT INTO VoteEntity(TAG, MOOD, TIME) values ('rainy day','UNHAPPY',1382257194186);  /*acum 4 zile*/
+INSERT INTO VoteEntity(TAG, MOOD, TIME) values ('rainy day','UNHAPPY',1382257194186);  /*acum 4 zile*/
+INSERT INTO VoteEntity(TAG, MOOD, TIME) values ('rainy day','UNHAPPY',1382257194186);  /*acum 4 zile*/
+INSERT INTO VoteEntity(TAG, MOOD, TIME) values ('rainy day','UNHAPPY',1382257194186);  /*acum 4 zile*/
+INSERT INTO VoteEntity(TAG, MOOD, TIME) values ('rainy day','UNHAPPY',1382257194186);  /*acum 4 zile*/
+
+
+INSERT INTO VoteEntity(TAG, MOOD, TIME) values ('monday','UNHAPPY',1382170794186); /*acum 5 zile*/
+INSERT INTO VoteEntity(TAG, MOOD, TIME) values ('monday','UNHAPPY',1382170794186); /*acum 5 zile*/
+INSERT INTO VoteEntity(TAG, MOOD, TIME) values ('monday','UNHAPPY',1382170794186); /*acum 5 zile*/
+INSERT INTO VoteEntity(TAG, MOOD, TIME) values ('monday','UNHAPPY',1382170794186); /*acum 5 zile*/
+INSERT INTO VoteEntity(TAG, MOOD, TIME) values ('monday','UNHAPPY',1382170794186); /*acum 5 zile*/
+INSERT INTO VoteEntity(TAG, MOOD, TIME) values ('monday','UNHAPPY',1382170794186); /*acum 5 zile*/
+INSERT INTO VoteEntity(TAG, MOOD, TIME) values ('monday','UNHAPPY',1382170794186); /*acum 5 zile*/
+INSERT INTO VoteEntity(TAG, MOOD, TIME) values ('monday','UNHAPPY',1382170794186); /*acum 5 zile*/
+INSERT INTO VoteEntity(TAG, MOOD, TIME) values ('monday','UNHAPPY',1382170794186); /*acum 5 zile*/
+INSERT INTO VoteEntity(TAG, MOOD, TIME) values ('monday','UNHAPPY',1382170794186); /*acum 5 zile*/
+INSERT INTO VoteEntity(TAG, MOOD, TIME) values ('monday','UNHAPPY',1382170794186); /*acum 5 zile*/
+
