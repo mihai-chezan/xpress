711bf08290dc94cbf814a1429f824aff87b0df63
sechelc <cristian.sechel@yahoo.ro>
2013-10-25 15:29:08 +0300

    added a pie chart per tag

---
+++ src/main/resources/assets/pieChart.js
@@ -0,0 +1,56 @@
+$(function () {
+
+    var lalala =  function(){
+        $.getJSON("http://localhost:9090/service/graphs/tags/"+s+"/LAST_MONTH", function(data){
+            chartOptions.series=data.series;
+            $('#container').highcharts(chartOptions);
+        })
+    }
+
+    var chartOptions = {
+        chart: {
+            plotBackgroundColor: null,
+            plotBorderWidth: null,
+            plotShadow: false
+        },
+        title: {
+            text: 'Browser market shares at a specific website, 2010'
+        },
+        tooltip: {
+            pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+        },
+        plotOptions: {
+            pie: {
+                allowPointSelect: true,
+                cursor: 'pointer',
+                dataLabels: {
+                    enabled: true,
+                    color: '#000000',
+                    connectorColor: '#000000',
+                    format: '<b>{point.name}</b>: {point.percentage:.1f} %'
+                }
+            }
+        },
+        series: [{
+            type: 'pie',
+            name: 'Browser share',
+            data: [
+                ['Firefox',   45.0],
+                ['IE',       26.8],
+                {
+                    name: 'Chrome',
+                    y: 12.8,
+                    sliced: true,
+                    selected: true
+                },
+                ['Safari',    8.5],
+                ['Opera',     6.2],
+                ['Others',   0.7]
+            ]
+        }]
+    }
+    lalala();
+
+});
+
+
